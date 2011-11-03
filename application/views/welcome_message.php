f619c8a3322b1dd92fc0a118b5e1a614c369fdfc
zhens <njuzhshao@gmail.com>
2011-11-03 07:58:03 -0700

    Adding Welcoming page.

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+	<title><?php echo $page_title?></title>
 
 	<style type="text/css">
 
@@ -67,19 +67,11 @@
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+	<h1>欢迎来到拇指姑娘。网站和应用建设中。</h1>
 
 	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div>
+	  <img src="<?php echo $static_img?>/muzhi.jpeg"/>
+    </div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
 </div>
